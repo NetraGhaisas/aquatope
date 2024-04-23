014234310891cb15beee525f56181951ba25d531
Netra Ghaisas <netraghaisas@gmail.com>
2024-04-23 17:29:31 -0400

    Read user timeline pipeline

--- benchmarks/social-network/openfaas/read-user-timeline-pipe/handler.go
+++ benchmarks/social-network/openfaas/read-user-timeline-pipe/handler.go
@@ -10,11 +10,15 @@ import (
 // Define provide definition of the workflow
 func Define(flow *faasflow.Workflow, context *faasflow.Context) (err error) {
 	flow.SyncNode().Modify(func(data []byte) ([]byte, error) {
-		data,_ = json.Marshal(data)
-		return data, nil
-	}).Apply("social-network-read-user-timeline").Modify(func(data []byte) ([]byte, error) {
-		return []byte(fmt.Sprintf("Function returned \"%s\"", string(data))), nil
-	})
+			data = []byte(string(data) + "modifier")
+			return data, nil
+		})
+	// flow.SyncNode().Modify(func(data []byte) ([]byte, error) {
+	// 	data,_ = json.Marshal(data)
+	// 	return data, nil
+	// }).Apply("social-network-read-user-timeline").Modify(func(data []byte) ([]byte, error) {
+	// 	return []byte(fmt.Sprintf("Function returned \"%s\"", string(data))), nil
+	// })
 	return
 }
 
